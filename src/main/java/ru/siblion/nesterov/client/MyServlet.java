7bc908f08f886c99696f7b2d90d2dc018f251dc4
alexander nesterov <alexander.nesterov@yahoo.com>
2017-01-18 14:04:32 +0300

    working doGet

--- src/main/java/ru/siblion/nesterov/client/MyServlet.java
+++ src/main/java/ru/siblion/nesterov/client/MyServlet.java
@@ -12,24 +12,33 @@ import java.io.PrintWriter;
  */
 public class MyServlet extends HttpServlet {
 
-    public void service(HttpServletRequest request,
-                        HttpServletResponse response) throws IOException {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        /*User user = new User("Alexander", "passwd");
+        request.setAttribute("user", user);*/
+        /*String string = "FROM SERVLET";
+        String username = request.getParameter("name");
+        request.setAttribute("username", username);
+        response.sendRedirect("index.jsp");*/
+
+
         // Must set the content type first
         response.setContentType("text/html");
         // Now obtain a PrintWriter to insert HTML into
         PrintWriter out = response.getWriter();
         out.println("<html><head><title>" +
-                "Hello World!</title></head>");
-        out.println("<body><h1>Hello World!</h1></body></html>");
+                "Get request!</title></head>");
+        out.println("<body><h1>Get request!</h1></body></html>");
+
+
     }
 
-    public void doGet(HttpServletRequest request,
+    public void doPost(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html><head><title>" +
-                "Hello World!</title></head>");
-        out.println("<body><h1>Hello World!</h1></body></html>");
+
     }
 
 }
