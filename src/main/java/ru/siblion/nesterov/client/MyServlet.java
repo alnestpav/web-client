d439958cfae0b6d0b9740a3fc60736fd575d6630
alexander nesterov <alexander.nesterov@yahoo.com>
2017-01-19 11:14:34 +0300

    working request from client to logreader

--- src/main/java/ru/siblion/nesterov/client/MyServlet.java
+++ src/main/java/ru/siblion/nesterov/client/MyServlet.java
@@ -1,12 +1,15 @@
 package ru.siblion.nesterov.client;
 
+import ru.siblion.nesterov.logreader.ws.*;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.datatype.XMLGregorianCalendar;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by alexander on 17.01.2017.
@@ -33,14 +36,27 @@ public class MyServlet extends HttpServlet {
         String location = request.getParameter("location");
         String dateFromString = request.getParameter("dateFrom");
         String dateToString = request.getParameter("dateTo");
-        String fileFormat = request.getParameter("fileFormat");
-
+        String fileFormatString = request.getParameter("fileFormat");
 
         System.out.println(string);
         System.out.println(location);
         System.out.println(dateFromString);
         System.out.println(dateToString);
-        System.out.println(fileFormat);
+        System.out.println(fileFormatString);
+
+        List<DateInterval> dateIntervals = new ArrayList<>();
+        DateInterval dateInterval = new DateInterval();
+        dateInterval.setDateFrom(dateFromString); // String преобразуется к XMLGregorianCalendar автоматически
+        dateInterval.setDateFrom(dateToString);
+
+        SoapWebService service = new SoapWebServiceService().getSoapWebServicePort();
+
+        Request clientRequest = new Request();
+        clientRequest.setString(string);
+        clientRequest.setLocation(location);
+        clientRequest.setFileFormat(FileFormat.fromValue(fileFormatString));
+        clientRequest.getDateIntervals().add(dateInterval);
+        Response clientResponse = service.getListOfLogMessages(clientRequest);
 
     }
 
