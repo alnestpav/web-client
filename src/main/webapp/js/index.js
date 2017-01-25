63fff5020855b4ec76ff2b24bae89539325ea06f
alexander nesterov <alexander.nesterov@yahoo.com>
2017-01-25 17:03:57 +0300

    change setColor function

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -27,7 +27,9 @@ document.getElementById("setColor").onclick = function() {
 
 
     rgbColor = hexToRgb(color);
-    if (rgbColor.r >= 230 && rgbColor.g >= 100 && rgbColor.b >= 200) {
+    hslColor = rgbToHsl(rgbColor.r, rgbColor.g, rgbColor.b);
+    alert("one " + hslColor[0] + "two " + hslColor[1] + "three " + hslColor[2]);
+    if ((hslColor[0] < 0.010 || hslColor[0] > 0.310) && hslColor[1] > 0.7 && hslColor[2] > 0.4) {
         alert("Pink color is not supported!")
     } else {
         document.body.style.background = color;
@@ -43,4 +45,25 @@ function hexToRgb(hex) {
         g: parseInt(result[2], 16),
         b: parseInt(result[3], 16)
     } : null;
+}
+
+function rgbToHsl(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if (max == min){
+        h = s = 0; // achromatic
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return [h, s, l];
 }
