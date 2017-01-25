b7f10c2235907ad89823a42f44b913f9f72876b2
alexander nesterov <alexander.nesterov@yahoo.com>
2017-01-25 13:23:32 +0300

    change urls

--- src/main/java/ru/siblion/nesterov/client/SigninServlet.java
+++ src/main/java/ru/siblion/nesterov/client/SigninServlet.java
@@ -5,21 +5,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
  * Created by alexander on 25.01.2017.
  */
-public class SignoutServlet extends HttpServlet {
+public class SigninServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-        session.invalidate();
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("signin.jsp");
-        requestDispatcher.forward(request, response);
+        if (request.getUserPrincipal() != null) {
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/search");
+            requestDispatcher.forward(request, response);
+        } else {
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("signin.jsp");
+            requestDispatcher.forward(request, response);
+        }
+
 
     }
 
